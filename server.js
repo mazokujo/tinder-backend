8792835a7300c8c83db4d0d3e413141e9e437b55
mazokujo <kabasele.jonathan@yahoo.fr>
2021-12-28 17:01:39 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import mongoose from 'mongoose'
+import Card from './dbCards.js'
+import Cors from 'cors'
+//App config
+const app = express();
+const port = process.env.PORT || 8001
+const connection_url = 'mongodb+srv://admin:C6EyrvKnelbJp0gO@cluster0.38jmq.mongodb.net/tinderdb?retryWrites=true&w=majority'
+//Middleware 
+app.use(express.json())
+app.use(Cors())
+//DB config
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+//API endpoint
+app.get('/', (req, res) => res.status(200).send('Hello world'))
+
+app.post('/tinder/card', (req, res) => {
+    const dbCard = req.body;
+    Card.create(dbCard, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+})
+app.get('/tinder/card', (req, res) => {
+    Card.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+})
+//listener
+app.listen(port, () => console.log(`listening on localhost ${port}`))
